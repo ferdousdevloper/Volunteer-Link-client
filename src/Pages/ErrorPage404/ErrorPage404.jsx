1ea2804ae7633ace0a5ffd522577c68b337a9a99
Your Nameferdousdevloper <eng.mfhossain@gmail.com>
2024-05-15 01:33:33 +0600

    error page implement

--- src/Pages/ErrorPage404/ErrorPage404.jsx
+++ src/Pages/ErrorPage404/ErrorPage404.jsx
@@ -6,7 +6,27 @@ import Lottie from "lottie-react";
 const ErrorPage404 = () => {
   return (
     <div className="h-[100vh]">
-      
+      <Helmet><title>404 ERROR</title></Helmet>
+      <section className="flex items-center h-full p-16 dark:bg-gray-50 dark:text-gray-800">
+        <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+          <div className="relative max-w-md text-center">
+          <Lottie 
+            animationData= {error404}
+            height={300}
+            width={300}
+            className=""
+            ></Lottie>
+            
+          </div>
+          <Link
+            to='/'
+              
+              className="absolute bottom-28 px-8 py-4 font-semibold rounded dark:bg-violet-600 dark:text-gray-50"
+            >
+              Back to homepage
+            </Link>
+        </div>
+      </section>
     </div>
   );
 };
